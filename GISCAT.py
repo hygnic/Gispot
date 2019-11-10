c168bef9fcd80201a556b205269cf06a1e675b29
hygnic <hygnic@outlook.com>
2019-11-10 19:02:55 +0800

    可执行文件

---
+++ GISCAT.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# User: liaochenchen, hygnic
+# Date: 2019/11/10
+
+import os,sys
+real = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(real,"bin"))
+# apppath = r"bin\appenter.py"
+print real
+import appenter
