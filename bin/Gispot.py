548944819281b9e1935874c5083100f733ad2bb9
hygnic <hygnic@outlook.com>
2019-12-02 16:43:26 +0800

    1202

--- bin/Gispot.py
+++ bin/Gispot.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 # User: liaochenchen, hygnic
 # Date: 2019/11/10
-import os,sys
-
+import os
+import sys
 """
 程序的入口，未以__file__为地址的文件的地址都以此入口为锚点
 """
 
-bin_p = os.path.dirname(__file__)
+bin_p = os.path.abspath(os.path.dirname(__file__))
 # real:  E:\move on move on\Gispot\bin
-root_p = os.path.dirname(bin_p)
+root_p = os.path.abspath(os.path.dirname(bin_p))
 # print "root_P:  ",root_p  #  E:/move on move on/Gispot
 sys.path.append(os.path.join(root_p, "GUIs"))
+print sys.path
 
 import tool_entrance
 
@@ -20,7 +21,6 @@ if __name__ == '__main__':
 	entrance = tool_entrance.AppEntrance()
 	entrance.menu()
 	entrance.rootwindow.mainloop()
-	
 	
 	
 	
