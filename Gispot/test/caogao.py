c4669a03dfc129166d8637509115b1581be7fe79
hygnic <hygnic@outlook.com>
2020-03-03 19:50:38 +0800

    20200303

--- Gispot/test/caogao.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# User: liaochenchen, hygnic
-# Date: 2019/12/26
-
-import os
-
-print os.path.isfile(r"../../docs/demo/gs_demo.mxd")
-print os.path.isfile(r"E:\move on move on\Gispot\docs\demo\gs_demo.mxd")
