dce47d9e32e797ec0ae4b1cd2a2e3a0f2a2bbb0c
hygnic <hygnic@outlook.com>
2020-07-23 17:42:53 +0800

    0723

--- GUIs/tooltk.py
+++ GUIs/tooltk.py
@@ -17,8 +17,9 @@ from GUIconfig import GUIpath
 # from GUIconfig.paths import GifPath
 from GUIconfig.GUIpath import PngIcon
 
-class GIF(object):
 
+class GIF(object):
+	
 	def __init__(self):
 		self.text = tk.PhotoImage(file=GUIpath.GifPath.textfile)
 		self.addfile = tk.PhotoImage(file=GUIpath.GifPath.add_file)
@@ -29,10 +30,12 @@ class GIF(object):
 		self.help = tk.PhotoImage(file=GUIpath.GifPath.info)
 		self.confirm = tk.PhotoImage(file=GUIpath.GifPath.confirm)
 		self.empty_1 = tk.PhotoImage(file=GUIpath.GifPath.empty1)
-		
-	# @staticmethod
-	# def dd():
-	# 	empty_1 = tk.PhotoImage(file=paths.GifPath.gif_empty1)
+
+
+# @staticmethod
+# def dd():
+# 	empty_1 = tk.PhotoImage(file=paths.GifPath.gif_empty1)
+
 
 class Tooltk(object):
 	"""工具的GUI界面"""
@@ -77,15 +80,16 @@ class Tooltk(object):
 	@property
 	def Frame(self):
 		return self.block_frame
+	
 	# static information box(Upper right corner)
 	@property
 	def FrameStatic(self):
 		return self.msgframe
+	
 	# dynamic information box(lower right corner)
 	@property
 	def FrameDynamic(self):
 		return self.major_msgframe
-	
 	
 	def color_mylife(self):
 		self.color1 = "#F1F1F1"  # help bar
@@ -128,7 +132,7 @@ class Tooltk(object):
 		self.frame_right_side = tk.Frame(self.window, width=496,
 										 border=0, relief="flat")
 		self.frame_right_side.pack(side="right",
-								  expand=True, fill="both")
+								   expand=True, fill="both")
 		# self.frame_right_side.propagate(False)
 		# 左边的主框
 		self.frame_left_side = tk.Frame(self.window, width=696,
@@ -147,17 +151,16 @@ class Tooltk(object):
 		# expand=False, fill ="x" 表示不会随着界面变大而变大，但是在
 		# x轴（左右）方向上会拉伸
 		# 左边主框中的帮助信息
-		help_f = tk.Frame(self.block_frame,
-						  relief=tk.GROOVE, bd=2, padx = 3)
+		help_f = tk.Frame(self.block_frame, relief=tk.GROOVE, bd=2, padx=3)
 		# help_f.pack_propagate(0)
-		help_f.pack(side=tk.BOTTOM, anchor="s",
-					expand=True, fill="both")
+		help_f.pack(side=tk.BOTTOM, anchor="s", expand=True, fill="both")
 		# 设置带滚动条的text
-		s_bar = tk.Scrollbar(help_f, relief="flat",
-							 elementborderwidth=-15)
+		s_bar = tk.Scrollbar(help_f, relief="flat", elementborderwidth=-15)
 		s_bar.pack(side="right", fill="y")
-		self.help_text = GUI.NeewwText(help_f, relief=tk.FLAT, height = 20,
-									   fg=self.color5, yscrollcommand=s_bar.set)
+		self.help_text = GUI.NeewwText(
+			help_f, relief=tk.FLAT, height=20,
+			fg=self.color5, yscrollcommand=s_bar.set
+		)
 		
 		self.help_text.pack(expand=True, fill="both")
 		s_bar.config(command=self.help_text.yview)
@@ -166,31 +169,34 @@ class Tooltk(object):
 			右边主框插入文本框，文本框分成上下两部分，上部分显示固定的信息，
 		下半部分显示动态信息"""
 		# 上栏
-		self.msgframe = stt.ScrolledText(self.frame_right_side, height="10",
-										 width="90")
+		self.msgframe = stt.ScrolledText(
+			self.frame_right_side, height="10", width="90")
 		# 不起作用，将所用txt都标记了
 		# self.text.tag_add("tag1","1.end","2.end")
-		self.msgframe.insert(tk.END,
-						 "Python 2.7.12 (v2.7.12:d33e0cf91556,Jun 27 2016, "
-						 "15:19:22) author: Liaochenchen 2019#00#00")  # ,"tag1"
+		self.msgframe.insert(
+			tk.END,
+			"Python 2.7.12 (v2.7.12:d33e0cf91556,Jun 27 2016, "
+			"15:19:22) author: Liaochenchen 2019#00#00"
+		)  # ,"tag1"
 		# self.text.tag_config("tag1",underline = True,foreground = "Ivory")
-		self.msgframe.pack(side="top", anchor="n", expand=True, fill="both",
-						   padx=2)
+		self.msgframe.pack(
+			side="top", anchor="n", expand=True, fill="both", padx=2)
 		# 下栏 主要的动态信息显示栏
-		s_bar = tk.Scrollbar(self.frame_right_side, relief="flat",
-							 elementborderwidth=-15)
+		s_bar = tk.Scrollbar(
+			self.frame_right_side, relief="flat", elementborderwidth=-15)
 		s_bar.pack(side="right", fill="y")
-		self.major_msgframe = GUI.NeewwText(self.frame_right_side, height="60",
-											yscrollcommand=s_bar.set)
+		self.major_msgframe = GUI.NeewwText(
+			self.frame_right_side, height="60", yscrollcommand=s_bar.set)
 		# 配置字体颜色
-		self.major_msgframe.tag_config("tag_1", backgroun="yellow",
-									   foreground="red", )
+		self.major_msgframe.tag_config(
+			"tag_1", backgroun="yellow", foreground="red", )
 		# 支持撤销操作，支持换行 wrap = "char"
 		# self.text_major_msg.insert(tk.END, ">>>" * 80)
-		self.major_msgframe.pack(side="top", anchor="n", expand=True,
-								 fill="both", padx=2)
+		self.major_msgframe.pack(
+			side="top", anchor="n", expand=True, fill="both", padx=2)
 		s_bar.config(command=self.major_msgframe.yview)
-		# return self.frame_major
+	
+	# return self.frame_major
 	
 	# Read help information and insert in help box
 	def read_help(self):
@@ -200,7 +206,6 @@ class Tooltk(object):
 				for read_line in read_msgs.readlines():
 					self.help_text.insert(tk.END, read_line)
 				self.help_text["state"] = "disabled"
-		
 	
 	def create_button(self):
 		"""
@@ -209,19 +214,21 @@ class Tooltk(object):
 		 2.button_quit: back button
 		 3.button_help: jump to a website which shows help information (most unused)
 		"""
-		self.button_confirm = GUI.HoverButton(self.frame_bottom_bar, msg="OK",
-											  image=self.gif_confirm,
-											  command=self.confirm_method,
-											  width =self._button_size,
-											  height =self._button_size)
+		self.button_confirm = GUI.HoverButton(
+			self.frame_bottom_bar, msg="OK",
+			image=self.gif_confirm,
+			command=self.confirm_method,
+			width=self._button_size,
+			height=self._button_size)
 		
 		# print "tooltk.py>>Border:", self.button_confirm["borderwidth"] # 2
 		# height = 18, width = 18,
 		# help button
-		self.button_help = GUI.HoverButton(self.frame_bottom_bar, msg="Info",
-										   image=self.gif_help,
-										   width=self._button_size,
-										   height=self._button_size)
+		self.button_help = GUI.HoverButton(
+			self.frame_bottom_bar, msg="Info",
+			image=self.gif_help,
+			width=self._button_size,
+			height=self._button_size)
 		
 		def __quit_inner():
 			"""
@@ -230,15 +237,16 @@ class Tooltk(object):
 			导致打开其他功能时找不到main_f而报错
 			"""
 			GUI.destroy_child(self.window)
+		
 		# Back button
-		self.button_quit = GUI.HoverButton(self.frame_bottom_bar, msg="Cancel",
-										   image=self.gif_quit,
-										   command=__quit_inner,
-										   width=self._button_size,
-										   height=self._button_size)
+		self.button_quit = GUI.HoverButton(
+			self.frame_bottom_bar, msg="Cancel",
+			image=self.gif_quit,
+			command=__quit_inner,
+			width=self._button_size,
+			height=self._button_size)
 		# pack
-		self.button_confirm.pack(side=tk.LEFT, anchor=tk.E,
-								 padx=5)
+		self.button_confirm.pack(side=tk.LEFT, anchor=tk.E, padx=5)
 		self.button_help.pack(side=tk.LEFT, anchor=tk.E, padx=5)
 		self.button_quit.pack(side=tk.RIGHT, anchor=tk.E, padx=5)
 		# buttonttk["command"] = self.kk
@@ -278,6 +286,7 @@ class Tooltk(object):
 		主Frame中的功能块之一，将通过Filedialog获取的 文件 传递更新给Entry,
 		同时可以获取 用户直接在Entry中输入的文件路径
 		"""
+		
 		# 文件选取菜单
 		def select_file():
 			file_path = tkFileDialog.askopenfilename(filetypes=sfb_filetype)
@@ -290,17 +299,19 @@ class Tooltk(object):
 		# 将Entry和按钮整齐的放到一起
 		frame_one = tk.Frame(self.block_frame)  # , border =1 ,relief = "raised"
 		frame_one.pack(side="top", anchor="center", expand=False, fill="x")
-	
+		
 		# Entry
 		input_msg1 = tk.StringVar()
-		self.input_sfb = GUI.NeewwEntry(frame_one, textvariable=input_msg1, border=0)
-		self.input_sfb.pack(side=tk.LEFT, anchor=tk.W, expand=True,
-							fill=tk.X, padx=10)
-		self.addfile_button = GUI.HoverButton(frame_one, text=u"选择",
-											  command=select_file,
-											  image=self.gif_addfile,
-											  width=self._button_size,
-											  height=self._button_size)
+		self.input_sfb = GUI.NeewwEntry(frame_one, textvariable=input_msg1,
+										border=0)
+		self.input_sfb.pack(
+			side=tk.LEFT, anchor=tk.W, expand=True, fill=tk.X, padx=10)
+		self.addfile_button = GUI.HoverButton(
+			frame_one, text=u"选择",
+			command=select_file,
+			image=self.gif_addfile,
+			width=self._button_size,
+			height=self._button_size)
 		self.addfile_button.pack(side=tk.RIGHT, anchor=tk.CENTER, padx=10)
 		return 1
 	
@@ -311,6 +322,7 @@ class Tooltk(object):
 		__sfb_filetype: tkFileDialog type
 		sfb_name: label name;ues to describe function
 		"""
+		
 		# 文件选取菜单
 		def select_file():
 			file_path = tkFileDialog.asksaveasfilename(filetypes=sfb_filetype)
@@ -327,17 +339,18 @@ class Tooltk(object):
 		frame_one.pack(side="top", anchor="center", expand=False, fill="x")
 		# 按钮
 		# photo = tk.PhotoImage(file=r"Icons/GenericBlackAdd32.png")
-		self.addfile_button = GUI.HoverButton(frame_one, image=self.gif_addfile,
-											  command= select_file,
-											  width= self._button_size,
-											  height = self._button_size)
+		self.addfile_button = GUI.HoverButton(
+			frame_one, image=self.gif_addfile,
+			command=select_file,
+			width=self._button_size,
+			height=self._button_size)
 		self.addfile_button.pack(side=tk.RIGHT, anchor=tk.CENTER, padx=10)
 		# Entry
 		input_msg1 = tk.StringVar()
-		self.input_sb = GUI.NeewwEntry(frame_one, textvariable=input_msg1,
-									   border=2, relief=tk.FLAT)
-		self.input_sb.pack(side=tk.LEFT, anchor=tk.W, expand=True,
-						   fill=tk.X, padx=15)
+		self.input_sb = GUI.NeewwEntry(
+			frame_one, textvariable=input_msg1, border=2, relief=tk.FLAT)
+		self.input_sb.pack(
+			side=tk.LEFT, anchor=tk.W, expand=True, fill=tk.X, padx=15)
 		return 1
 	
 	def single_dir_block(self, sdb_name):
@@ -346,6 +359,7 @@ class Tooltk(object):
 		主Frame中的功能块之一，将通过Filedialog获取的 文件夹  传递更新给Entry,
 		同时可以获取 用户直接在Entry中输入的文件路径
 		"""
+		
 		def select_file():
 			# global file_path
 			file_path = tkFileDialog.askdirectory()
@@ -359,15 +373,18 @@ class Tooltk(object):
 		frame_one.pack(side="top", anchor="center", expand=False, fill="x")
 		# Entry
 		input_msg1 = tk.StringVar()
-		self.input_sdb = GUI.NeewwEntry(frame_one, textvariable=input_msg1, bd = 0)
-		self.input_sdb.pack(side=tk.LEFT, anchor=tk.W, expand=True, fill=tk.X,
-							padx=10)
+		self.input_sdb = GUI.NeewwEntry(
+			frame_one, textvariable=input_msg1, bd=0)
+		self.input_sdb.pack(
+			side=tk.LEFT, anchor=tk.W, expand=True, fill=tk.X, padx=10)
 		# input_msg.set(one_file_path)
-		self.addfile_button = GUI.HoverButton(frame_one,
-											  command=select_file,
-											  image=self.gif_folder,
-											  width = self._button_size,
-											  height = self._button_size)
+		self.addfile_button = GUI.HoverButton(
+			frame_one,
+			command=select_file,
+			image=self.gif_folder,
+			width=self._button_size,
+			height=self._button_size)
+		
 		self.addfile_button.pack(side=tk.RIGHT, anchor=tk.CENTER, padx=10)
 		return 1
 	
@@ -391,16 +408,16 @@ class Tooltk(object):
 		frame_one.pack(side="top", anchor="center", expand=False, fill="x")
 		# Entry
 		input_msg1 = tk.StringVar()
-		self.input_sdb2 = GUI.NeewwEntry(frame_one,
-										 textvariable=input_msg1, bd=0)
-		self.input_sdb2.pack(side=tk.LEFT, anchor=tk.W, expand=True, fill=tk.X,
-							padx=10)
+		self.input_sdb2 = GUI.NeewwEntry(frame_one, textvariable=input_msg1, bd=0)
+		self.input_sdb2.pack(
+			side=tk.LEFT, anchor=tk.W, expand=True, fill=tk.X, padx=10)
 		# input_msg.set(one_file_path)
-		self.addfile_button = GUI.HoverButton(frame_one,
-											  command=select_file,
-											  image=self.gif_folder,
-											  width=self._button_size,
-											  height=self._button_size)
+		self.addfile_button = GUI.HoverButton(
+			frame_one,
+			command=select_file,
+			image=self.gif_folder,
+			width=self._button_size,
+			height=self._button_size)
 		self.addfile_button.pack(side=tk.RIGHT, anchor=tk.CENTER, padx=10)
 		return 1
 	
@@ -427,10 +444,11 @@ class Tooltk(object):
 							expand=True, fill=tk.X, padx=10)
 		# input_msg.set(one_file_path)
 		# 按钮
-		int_button_1 = GUI.HoverButton(frame_one, image=self.gif_empty_1,
-									   state = "disabled",
-									   width = self._button_size,
-									   height = self._button_size)
+		int_button_1 = GUI.HoverButton(
+			frame_one, image=self.gif_empty_1,
+			state="disabled",
+			width=self._button_size,
+			height=self._button_size)
 		int_button_1.pack(side=tk.RIGHT, anchor=tk.CENTER, padx=10)
 		return 1
 	
@@ -468,18 +486,18 @@ class Tooltk(object):
 		# input_msg.set(one_file_path)
 		return 1
 	
-	def single_text_block(self,stb_name):
+	def single_text_block(self, stb_name):
 		"""
 		text组件
 		:param stb_name:
 		:return:
 		"""
 		# 块名称
-		tk.Label(self.block_frame, text=stb_name).\
+		tk.Label(self.block_frame, text=stb_name). \
 			pack(side=tk.TOP, anchor=tk.NW, padx=40)
 		frame_one = tk.Frame(self.block_frame)
 		frame_one.pack(side="top", anchor="center",
-					  expand = True,fill = "both")
+					   expand=True, fill="both")
 		# 右边障碍要素
 		GUI.HoverButton(
 			frame_one, state="disabled",
@@ -497,10 +515,11 @@ class Tooltk(object):
 		
 		# frame_one.columnconfigure(0, weight=1)
 		# frame_one.columnconfigure(1, weight=1)
-		self.input_tb = GUI.NeewwText(frame_one, wrap ="none",
-									  relief = "flat", height = 10)
+		self.input_tb = GUI.NeewwText(frame_one, wrap="none",
+									  relief="flat", height=10)
 		self.input_tb.pack(expand=True, fill="both")
-		# text.grid(column = 0,sticky = "nesw")
+	
+	# text.grid(column = 0,sticky = "nesw")
 	
 	def divider_bar_block(self, master, color11, color22):
 		"""
@@ -508,10 +527,10 @@ class Tooltk(object):
 		:return:
 		"""
 		s = GUI.GradientCanvas(
-			master,color11,color22,
-			height= 10,bd = 0
-		).pack(fill = "x")
-		
+			master, color11, color22,
+			height=10, bd=0
+		).pack(fill="x")
+	
 	def get_blockvalue(self, *arg):
 		"""
 		列表初始化
@@ -524,8 +543,8 @@ class Tooltk(object):
 			# 由于Entry输出纯英文数字时是str格式，为方便后续进行比较等操作
 			# 将str转换为unicode
 			msg = i.get()
-			print "msg:",msg
-			print "msg's type:",type(msg)
+			print "msg:", msg
+			print "msg's type:", type(msg)
 			if type(msg) == type("str"):  # unicode
 				msg = msg.decode("cp936")
 				print "msg:", msg
@@ -541,14 +560,14 @@ class Tooltk(object):
 		return self.block_list
 
 
-
 class SingleFileBlock(object):
 	"""单个文件选择功能块
 	ss = tooltk.SingleFileBlock(frame, "添加文件",
 									tkFileDialog.askopenfilename，[(u'文本文档', '*.txt'), ('All Files', '*')],
 									"add_file")"""
 	_button_pixel_size = 24
-	def __init__(self, frames, name,tkFileDialogFunc,filetype,image):
+	
+	def __init__(self, frames, name, tkFileDialogFunc, filetype, image):
 		"""
 		:param frames: {Tuple} GUI界面中几个主要框架
 			*self.block_frame,self.msgframe,self.major_msgframe
@@ -571,7 +590,7 @@ class SingleFileBlock(object):
 			* "add_file"
 		"""
 		# 传入参数
-		self.__master = frames[0] # self.frame_major
+		self.__master = frames[0]  # self.frame_major
 		self.__static = frames[1]
 		self.__dymnic = frames[2]
 		self.__sfb_filetype = filetype
@@ -585,7 +604,7 @@ class SingleFileBlock(object):
 		self.single_file_block(name)
 	
 	# 设置图片,使用了反射
-	 # 方案一：使用gif图片
+	# 方案一：使用gif图片
 	# def image(self,image):
 	# 	# print "getattr(GifPath,image):",getattr(GifPath,image)
 	# 	raw_p = getattr(GifPath,image)
@@ -603,8 +622,7 @@ class SingleFileBlock(object):
 		# global a_gif
 		# a_gif = tk.PhotoImage(file=raw_p)
 		self.but_image = photo
-
-
+	
 	# 打开 文件/文件夹 选取窗口
 	def dialog(self):
 		file_path = self.dialoge_type(filetypes=self.__sfb_filetype)
@@ -629,15 +647,16 @@ class SingleFileBlock(object):
 		frame_one.pack(side="top", anchor="center", expand=False, fill="x")
 		
 		# Entry
-		self.__newEntry = GUI.NeewwEntry(frame_one,
-										 textvariable=self.var, border=0)
-		self.__newEntry.pack(side=tk.LEFT, anchor=tk.W, expand=True,
-							 fill=tk.X, padx=10)
-		self.__button = GUI.HoverButton(frame_one, text=u"选择",
-										command=self.dialog,
-										image = self.but_image,
-										width=self._button_pixel_size,
-										height=self._button_pixel_size)
+		self.__newEntry = GUI.NeewwEntry(
+			frame_one, textvariable=self.var, border=0)
+		self.__newEntry.pack(
+			side=tk.LEFT, anchor=tk.W, expand=True, fill=tk.X, padx=10)
+		self.__button = GUI.HoverButton(
+			frame_one, text=u"选择",
+			command=self.dialog,
+			image=self.but_image,
+			width=self._button_pixel_size,
+			height=self._button_pixel_size)
 		self.__button.pack(side=tk.RIGHT, anchor=tk.CENTER, padx=10)
 	
 	# 点击确认键的时候获取Entry中的值
@@ -646,7 +665,7 @@ class SingleFileBlock(object):
 		# 由于Entry输出纯英文数字时是str格式，为方便后续进行比较等操作
 		# 将str转换为unicode
 		msg = self.__newEntry.get()
-		frame  = self.__static
+		frame = self.__static
 		print "msg", msg
 		print "msg's type", type(msg)
 		if type(msg) == type("str"):  # unicode
@@ -664,10 +683,11 @@ class SingleFileBlock(object):
 			# print "msg's type2", type(msg)
 			# print os.path.isdir(msg)
 			return msg
+	
 	# def get(self):
 	# 	_value = _handlevalue(self.__newEntry, self.__static)
 	# 	return _value
-
+	
 	@property
 	def button(self):
 		return self.__button
@@ -678,23 +698,31 @@ class SingleFileBlock(object):
 
 
 # 文件夹模块
-def blockDIR_in(frames,name):
-	return SingleFileBlock(frames, name,tkFileDialog.askdirectory,None,"folder1")
+def blockDIR_in(frames, name):
+	return SingleFileBlock(
+		frames, name, tkFileDialog.askdirectory, None, "folder1")
 
-def blockSheet(frames,name):
-	return SingleFileBlock(frames, name,tkFileDialog.askopenfilename,[(u'工作簿', '*.xlsx'),(u'工作簿', '*.xls'), ('All Files', '*')],"add_file")
+
+def blockSheet(frames, name):
+	return SingleFileBlock(
+		frames, name, tkFileDialog.askopenfilename,
+		[(u'工作簿', '*.xlsx'), (u'工作簿', '*.xls'),
+		('All Files', '*')], "add_file"
+	)
+
 
 def blockDIR_out(frames, name):
-	return SingleFileBlock(frames, name, tkFileDialog.askdirectory, None,
-						   "folder2")
+	return SingleFileBlock(
+		frames, name, tkFileDialog.askdirectory, None, "folder2")
+
+
 # 数字输入模块（没有button）
 def blockValue(frames, name):
-	inner = SingleFileBlock(frames, name, tkFileDialog.askdirectory, None,
-						   "empty")
+	inner = SingleFileBlock(frames, name, tkFileDialog.askdirectory, None ,"empty")
 	# inner.block_button["state"] ="disabled"  #不行
 	# inner.block_button.config(state ="disabled") # 不行
 	# 解除绑定
-	inner.button.close() # state = "disabled",  normal,active
+	inner.button.close()  # state = "disabled",  normal,active
 	return inner
 
 
@@ -714,9 +742,10 @@ if __name__ == '__main__':
 			self.single_dir_block(u"图片文件夹")
 			frame = (self.block_frame, self.msgframe, self.major_msgframe)
 			SingleFileBlock(frame, u"TEST",
-							tkFileDialog.askopenfilename,[(u'文本文档', '*.txt'),
-														  ('All Files', '*')],
-														 "add_file")
+							tkFileDialog.askopenfilename, [(u'文本文档', '*.txt'),
+														   ('All Files', '*')],
+							"add_file")
+		
 		# self.addfile_button["state"] = "disabled"
 		# self.addfile_button.pack_forget() # 隐藏模块
 		# self.addfile_button.destroy()	# 隐藏模块
@@ -741,5 +770,7 @@ if __name__ == '__main__':
 			filedialog: E:/move on move on/公示图  type:  <type 'unicode'>
 			结论:
 			"""
+	
+	
 	app = TstApp()
 	app.window.mainloop()
