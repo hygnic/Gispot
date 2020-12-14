ab9f90737b577a34f28c822c63f9d0ff27548de9
hygnic <hygnic@outlook.com>
2020-12-14 21:31:12 +0800

    1214

--- gispot/test/自动隐藏显示滚动条_pack.py
+++ gispot/test/自动隐藏显示滚动条_pack.py
@@ -3,9 +3,9 @@
 # Date: 2020/3/13
 # python2
 
-from Tkinter import *
+import Tkinter as tk
 
-class AutoScrollbar(Scrollbar):
+class AutoScrollbar(tk.Scrollbar):
     # a scrollbar that hides itself if it's not needed.  only
     # works if you use the pack geometry manager.
     def set(self, lo, hi):
@@ -13,24 +13,26 @@ class AutoScrollbar(Scrollbar):
             # grid_remove is currently missing from Tkinter!
             self.pack_forget()
         else:
-            if self.cget("orient") == HORIZONTAL:
-                self.pack(fill=X)
+            if self.cget("orient") == tk.HORIZONTAL: # 将字符串转变为关键字
+                self.pack(fill=tk.X)
             else:
-                self.pack(fill=Y)
-        Scrollbar.set(self, lo, hi)
+                self.pack(fill=tk.Y)
+        tk.Scrollbar.set(self, lo, hi)
     def grid(self, **kw):
-        raise TclError, "cannot use grid with this widget"
+        raise tk.TclError, "cannot use grid with this widget"
     def place(self, **kw):
-        raise TclError, "cannot use place with this widget"
+        raise tk.TclError, "cannot use place with this widget"
 
 # create scrolled canvas
 
-root = Tk()
+root = tk.Tk()
 
-hscrollbar = AutoScrollbar(root, orient=HORIZONTAL)
-canvas = Canvas(root,
+hscrollbar = AutoScrollbar(root, orient=tk.HORIZONTAL)
+# vscrollbar = AutoScrollbar(root, orient=tk.VERTICAL)
+
+canvas = tk.Canvas(root,
                 xscrollcommand=hscrollbar.set)
-canvas.pack(side=TOP, fill=BOTH, expand=True)
+canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 hscrollbar.pack()
 
 hscrollbar.config(command=canvas.xview)
@@ -41,17 +43,17 @@ root.grid_columnconfigure(0, weight=1)
 
 # create canvas contents
 
-frame = Frame(canvas)
+frame = tk.Frame(canvas)
 frame.rowconfigure(1, weight=1)
 frame.columnconfigure(1, weight=1)
 
 rows = 5
 for i in range(1,rows):
     for j in range(1,10):
-        button = Button(frame, padx=7, pady=7, text="[%d,%d]" % (i,j))
+        button = tk.Button(frame, padx=7, pady=7, text="[%d,%d]" % (i,j))
         button.grid(row=i, column=j, sticky='news')
 
-canvas.create_window(0, 0, anchor=NW, window=frame)
+canvas.create_window(0, 0, anchor=tk.NW, window=frame)
 
 frame.update_idletasks()
 
