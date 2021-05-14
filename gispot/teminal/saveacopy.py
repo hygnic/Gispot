65bafde2e2c027c8ebf61d498227e712736a1bdc
hygnic <hygnic@outlook.com>
2021-05-14 14:50:08 +0800

    0514

---
+++ gispot/teminal/saveacopy.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+# -------------------------------------------
+# Name:              saveacopy
+# Author:            Hygnic
+# Created on:        2021/5/13 9:25
+# Version:           AecGIS10.3 Python2.7.8
+# Reference:         
+"""
+Description:         重命名多个图层的名称
+Usage:               
+"""
+# -------------------------------------------
+from __future__ import unicode_literals
+from __future__ import absolute_import
+import arcpy
+import os
+
+cws = os.getcwd()
+mxds = [x for x in os.listdir(cws) if x[-3:] == "mxd"]
+
+for item in mxds:
+    name, suff = os.path.splitext(item)
+    print name
+    name = name.decode("cp936")
+    mxd = arcpy.mapping.MapDocument(os.path.join(cws, item))
+    mxd.saveACopy(name+"9.3.mxd", version="9.3")
+    del mxd
