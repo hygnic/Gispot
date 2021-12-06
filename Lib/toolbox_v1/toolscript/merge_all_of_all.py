4ba9d9e2a40de79e925586e8ae2d5107a4b90b29
hygnic <hygnic@outlook.com>
2021-12-06 17:27:18 +0800

    20211206

---
+++ Lib/toolbox_v1/toolscript/merge_all_of_all.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# -------------------------------------------
+# Name:              merge_all_of_all
+# Author:            Hygnic
+# Created on:        2021/11/30 16:49
+# Version:           
+# Reference:         
+"""
+Description:         将一个（多个）图层合并成一个图层
+                    并融合成一整块，消除所有重叠和属性信息
+Usage:               
+"""
+# -------------------------------------------
+import arcpy
